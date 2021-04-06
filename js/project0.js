a6cd7e6f9aafcf92c887e4103f308a5d56405609
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-04-06 14:10:22 +1000

    first version

---
+++ js/project0.js
@@ -0,0 +1,20 @@
+
+
+1. userChoice
+2. computerChoice
+3. Creat the X and O
+4. Create all winning combinations
+
+[0, 1, 2]
+[3, 4, 5]
+[6, 7, 8]
+[0, 3, 6]
+[1, 4, 7]
+[2, 5, 8]
+[0, 4, 8]
+[2, 4, 7]
+
+5. Take it in turns to play
+6. Stop the game when there is a winner
+7. Show winning combination
+8. Get the computer to think strategically
